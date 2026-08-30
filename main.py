199781a3f74e7b904981c8c047645d26c15e762e
Josie <switchrootjose@gmail.com>
2026-08-30 18:27:17 -0400

    fix static directory crash - hopefully
    Comment out the static files mount in FastAPI app

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
-app.mount("/static", StaticFiles(directory="static"), name="static")
+#app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 async def run_scrape_all():
